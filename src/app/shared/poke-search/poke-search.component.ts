6a5e2da04006b22275793057e7e2d92ce184928f
Vinicius Moreira <vmarv@hotmail.com>
2023-07-27 21:39:47 -0300

    project complete

---
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'poke-search',
+  templateUrl: './poke-search.component.html',
+  styleUrls: ['./poke-search.component.scss']
+})
+export class PokeSearchComponent implements OnInit {
+
+    @Output() emmitSearch: EventEmitter<string> = new EventEmitter();
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    search(value: string) {
+        this.emmitSearch.emit(value);
+    }
+
+}
